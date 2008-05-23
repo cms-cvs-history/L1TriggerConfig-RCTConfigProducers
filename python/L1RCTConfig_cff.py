0872a70c213440a3c69db88bfdf728de278f9262
cmsbuild <>
2008-05-23 14:12:33 +0000

    This commit was manufactured by cvs2git to create tag 'L1TriggerConfig- RCTConfigProducers-V00-00-18'.
    Sprout from master 2008-05-23 13:41:47 UTC Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch> 'Adding channel mask record in .cff'
    Cherrypick from master 2008-05-23 14:12:32 UTC Jessica Lynn Leonard <sha1-2c6a3e8f6f1bdddd9b6f355c6bc97fbef5175448@cern.ch> 'Fixing _cff.py for tag V00-00-17':
        python/L1RCTConfig_cff.py
    Delete:
        data/L1RctInputScalesProducer.cfi
        python/L1RctInputScalesProducer_cfi.py
        src/L1RctInputScalesProducer.cc

--- python/L1RCTConfig_cff.py
+++ python/L1RCTConfig_cff.py
@@ -4,7 +4,11 @@ from CalibCalorimetry.EcalTPGTools.ecalTPGScale_cff import *
 
 from L1TriggerConfig.RCTConfigProducers.L1RCTConfig_cfi import *
 
-from L1TriggerConfig.RCTConfigProducers.L1RctInputScalesProducer_cfi import *
+l1RctMaskRcds = cms.ESSource("EmptyESSource",
+                             recordname = cms.string('L1RCTChannelMaskRcd'),
+                             iovIsRunNotTime = cms.bool(True),
+                             firstValid = cms.vuint32(1)
+                             )
 
 l1RctParamsRecords = cms.ESSource("EmptyESSource",
                                   recordName = cms.string('L1RCTParametersRcd'),
--- python/L1RctInputScalesProducer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-L1RctInputScalesProducer = cms.ESProducer("L1RctInputScalesProducer")
-
-
-
